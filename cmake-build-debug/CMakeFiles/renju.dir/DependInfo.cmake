0b212c778b519416fd6ed93140742a12f3de4c70
fenghe <jxphxufh@gmail.com>
2019-06-11 00:06:26 +0800

    initial commit

---
+++ cmake-build-debug/CMakeFiles/renju.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/fenghexu/OneDrive - zju.edu.cn/Documents/Helping other people/张佳娉/renju/main.cpp" "/Users/fenghexu/OneDrive - zju.edu.cn/Documents/Helping other people/张佳娉/renju/cmake-build-debug/CMakeFiles/renju.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk/System/Library/Frameworks/OpenGL.framework"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/renju.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/renju.dir/main.cpp.o"
+  "renju.pdb"
+  "renju"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/renju.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
